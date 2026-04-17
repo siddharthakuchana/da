4273ed4824fd4bfd60e1f3e04865e8e0f0a1080e
Siddhartha Kuchana <siddharthakuchana0207@gmail.com>
2026-04-17 18:29:35 +0530

    Add Random Forest Classifier for disease prediction
    Implemented a Random Forest Classifier to predict disease based on age, blood pressure, and cholesterol levels. The model is trained on a sample dataset and provides accuracy and confusion matrix outputs.

---
+++ RandoforestClassifier.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, confusion_matrix
+data = {
+    'Age': [25, 45, 35, 50, 23, 40, 60, 55, 30, 48],
+    'BP': [120, 140, 130, 150, 110, 135, 160, 155, 125, 145],
+    'Cholesterol': [200, 240, 220, 260, 180, 230, 270, 250, 210, 245],
+    'Disease': [0, 1, 0, 1, 0, 1, 1, 1, 0, 1]   # 1 = Yes, 0 = No
+}
+df=pd.DataFrame(data)
+print("dataset:\n",df)
+
+X=df[['Age','BP','Cholesterol']]
+y=df['Disease']
+X_train,X_test,y_train,y_test=train_test_split(X, y, test_size=0.4, random_state=42)
+model=RandomForestClassifier(max_depth=3)
+model.fit(X_train,y_train)
+y_pred=model.predict(X_test)
+accuracy=accuracy_score(y_test,y_pred)
+print("Accuracy: ",accuracy)
+cm=confusion_matrix(y_test,y_pred)
+print("Confusion Matrix:\n",cm)
+age=int(input("enter the age: "))
+bp=int(input("enter the bp: "))
+cholesterol=int(input("enter the cholesterol: "))
+prediction=model.predict([[age,bp,cholesterol]])
+if prediction[0]==1:
+    print("Disease: Yes")
+else:
+    print("Disease: No")
